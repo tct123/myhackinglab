70b32481902acd5476cbc8d671b75f52796e5d9b
tct123 <42028373+tct123@users.noreply.github.com>
2023-12-29 13:25:34 +0100

    Update app.py

--- src/myhackinglab/app.py
+++ src/myhackinglab/app.py
@@ -19,6 +19,7 @@ class MyHackingLab(toga.App):
         show the main window.
         """
         main_box = toga.Box()
+        main_box.style.direction = "column"
         
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
