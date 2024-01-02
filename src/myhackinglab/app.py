139b33c5edafad11d22b47baf36a2fffd3212488
tct123 <42028373+tct123@users.noreply.github.com>
2024-01-02 07:20:49 +0100

    Update app.py

--- src/myhackinglab/app.py
+++ src/myhackinglab/app.py
@@ -9,7 +9,6 @@ import phonenumbers as pn
 
 
 class MyHackingLab(toga.App):
-
     def startup(self):
         """
         Construct and show the Toga application.
@@ -20,25 +19,41 @@ class MyHackingLab(toga.App):
         """
         main_box = toga.Box()
         main_box.style.direction = "column"
-        
+
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
-        btn1 = toga.Button(text="Phonenumber Location", on_press=self.btn_phonenumber_location)
+        btn1 = toga.Button(
+            text="Phonenumber Location", on_press=self.btn_phonenumber_location
+        )
         main_box.add(btn1)
+
     def leave(self, widget):
         self.exit()
-    def btn_phonenumber_location(self,widget):
+
+    def btn_phonenumber_location(self, widget):
         box = toga.Box()
         box.style.direction = "column"
-        self.window = toga.Window(title="Phonenumber Location")
-        self.window.content = box
-        self.window.show()
-        number = toga.TextInput(placeholder="Phonenumber (international)")
-        checkbutton = toga.Button(text="Check")
+        window = toga.Window(title="Phonenumber Location")
+        window.content = box
+        window.show()
+        self.number = toga.TextInput(placeholder="Phonenumber (international)")
+        checkbutton = toga.Button(text="Check",on_press=self.phonenumber_screen)
 
-        box.add(number)
+        box.add(self.number)
+        box.add(checkbutton)
+
+    def phonenumber_data():
+        data = []
+        return data
+
+    def phonenumber_screen(self, widget):
+        box = toga.Box()
+        window = toga.Window("Phonenumber")
+        window.content = box
+        window.show()
+        p
 
 
 def main():
