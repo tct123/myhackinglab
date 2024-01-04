520f8fab78c617cc70ea379eb7fbe14793e4001e
tct123 <42028373+tct123@users.noreply.github.com>
2024-01-04 14:34:05 +0100

    Update image.py

--- image.py
+++ image.py
@@ -1,10 +1,25 @@
+# gps_exif_getter.py
+# https://www.blog.pythonlibrary.org/2021/01/13/getting-gps-exif-data-with-python/
 from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
 
 
-def gpslocation(file):
-    data = file
-    return data
+def get_exif(image_file_path):
+    exif_table = {}
+    image = Image.open(image_file_path)
+    info = image.getexif()
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        exif_table[decoded] = value
+
+    gps_info = {}
+    for key in exif_table['GPSInfo'].keys():
+        decode = GPSTAGS.get(key,key)
+        gps_info[decode] = exif_table['GPSInfo'][key]
+
+    return gps_info
 
 
 if __name__ == "__main__":
-    gpslocation()
+    exif = get_exif("jester.jpg")
+    print(exif)
