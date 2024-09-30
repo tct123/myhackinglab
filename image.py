0ba2ffe331cc9237097479bfafaf65574cedc29e
tct123 <42028373+tct123@users.noreply.github.com>
2024-09-30 21:56:59 +0200

    Update image.py

--- image.py
+++ image.py
@@ -1,25 +1,28 @@
-# gps_exif_getter.py
-# https://www.blog.pythonlibrary.org/2021/01/13/getting-gps-exif-data-with-python/
-from PIL import Image
-from PIL.ExifTags import TAGS, GPSTAGS
+# from exif import Image
 
 
 def get_exif(image_file_path):
-    exif_table = {}
-    image = Image.open(image_file_path)
-    info = image.getexif()
-    for tag, value in info.items():
-        decoded = TAGS.get(tag, tag)
-        exif_table[decoded] = value
-
-    gps_info = {}
-    for key in exif_table[1][1].keys():
-        decode = GPSTAGS.get(key, key)
-        gps_info[decode] = exif_table["GPSInfo"][key]
-
-    return gps_info
+    with open(image_file_path, "rb") as image_file:
+        my_image = Image(image_file)
+    if my_image.has_exif:
+        return my_image.list_all()
+    else:
+        return None
 
 
 if __name__ == "__main__":
-    exif = get_exif("WindowsXP.jpg")
-    print(exif)
+    # exif = get_exif("WindowsXP.jpg")
+    # print(exif)
+    from PIL import Image, ExifTags
+
+    img = Image.open("WindowsXP.jpg")
+    img_exif = img.getexif()
+
+    if img_exif is None:
+        print('Sorry, image has no exif data.')
+    else:
+        for key, val in img_exif.items():
+            if key in ExifTags.TAGS:
+                print(f'{ExifTags.TAGS[key]}:{val}')
+            else:
+                print(f'{key}:{val}')
